9ef3ea2a58c6c133ec10ea753957d47f8308b992
RiviereGregory <gregory.riviere@laposte.net>
2023-09-20 21:07:07 +0200

    feat: OS Android - Ajout nouvelle activité avec utilisation

--- Chapitre3Activity/app/src/main/java/mrs/riverjach/chapitre3activity/MainActivity.kt
+++ Chapitre3Activity/app/src/main/java/mrs/riverjach/chapitre3activity/MainActivity.kt
@@ -1,11 +1,18 @@
 package mrs.riverjach.chapitre3activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.start_activity)
+        button.setOnClickListener {
+            val intent = Intent(this, Activity2::class.java)
+            startActivity(intent)
+        }
     }
 }
