c339cf55bd480cc75416f9bea2a4545f5f7c3e1f
RiviereGregory <gregory.riviere@laposte.net>
2023-10-06 18:33:37 +0200

    feat: OS Android - Exemple interaction avec checkBox pour affichage imageView

--- Chapitre3Activity/app/src/main/java/mrs/riverjach/chapitre3activity/ComposantGraphics.kt
+++ Chapitre3Activity/app/src/main/java/mrs/riverjach/chapitre3activity/ComposantGraphics.kt
@@ -1,11 +1,25 @@
 package mrs.riverjach.chapitre3activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ComposantGraphics : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_composant_graphics)
+        val monImageView: ImageView = findViewById(R.id.monImage)
+        val maCheckBox: CheckBox = findViewById(R.id.maCheckBox)
+
+        monImageView.visibility = View.INVISIBLE
+        maCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                monImageView.visibility = View.VISIBLE
+            } else {
+                monImageView.visibility = View.INVISIBLE
+            }
+        }
     }
 }
