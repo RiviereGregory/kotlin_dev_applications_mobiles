71ea6b9f519347c5210a7ecf0322f833fb16bf98
RiviereGregory <gregory.riviere@laposte.net>
2023-11-10 17:53:57 +0100

    feat: BlocNote - Init

---
+++ BlocNote/app/src/main/java/mrs/riverjach/blocnote/NotesListe.kt
@@ -0,0 +1,11 @@
+package mrs.riverjach.blocnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.listes_notes)
+    }
+}
