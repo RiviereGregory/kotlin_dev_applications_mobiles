2a812e874c854a749da81733fb36e8ddaf6ee724
RiviereGregory <gregory.riviere@laposte.net>
2024-01-12 18:04:35 +0100

    feat: AllerPlusLoinAvecKotlin - fonction d extension Custom 2

--- AllerplusloinavecKotlin/app/src/main/java/mrs/riverjach/allerplusloinaveckotlin/MainActivity.kt
+++ AllerplusloinavecKotlin/app/src/main/java/mrs/riverjach/allerplusloinaveckotlin/MainActivity.kt
@@ -27,6 +27,12 @@ class MainActivity : AppCompatActivity() {
         return this % 2 == 0
     }
 
+    fun MutableList<Int>.swap(index1: Int, index2: Int) {
+        val tmp = this[index1] // 'this' corresponds to the list
+        this[index1] = this[index2]
+        this[index2] = tmp
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
@@ -76,6 +82,16 @@ class MainActivity : AppCompatActivity() {
         println(SEPARATOR)
         println("###### Custom 1 #####")
         fonctionExtensionCustom1()
+        println("###### Custom 2 #####")
+        fonctionExtensionCustom2()
+    }
+
+    private fun fonctionExtensionCustom2() {
+        val maList = mutableListOf<Int>(1, 2, 5, 10, 20, 40, 100)
+        println(maList)
+        maList.swap(0, 3)
+        println("Swap list index 0 and 3")
+        println(maList)
     }
 
     private fun fonctionExtensionCustom1() {
