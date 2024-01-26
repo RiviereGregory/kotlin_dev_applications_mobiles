4e8485aa35d7d5bc1ded6bb8f66088322a456db4
RiviereGregory <gregory.riviere@laposte.net>
2024-01-26 18:33:43 +0100

    feat: AllerPlusLoinAvecOSAndroid - utilisation Fragments

---
+++ AllerplusloinavecKotlin/app/src/main/java/mrs/riverjach/allerplusloinaveckotlin/fragments/GreyDarkFragment.kt
@@ -0,0 +1,18 @@
+package mrs.riverjach.allerplusloinaveckotlin.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import mrs.riverjach.allerplusloinaveckotlin.R
+
+class GreyDarkFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_greydark, container, false)
+    }
+}
