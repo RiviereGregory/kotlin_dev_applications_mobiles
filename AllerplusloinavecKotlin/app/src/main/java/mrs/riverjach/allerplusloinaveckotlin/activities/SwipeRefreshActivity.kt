248fcb5f36c74839b38b85c78219e2326c982993
RiviereGregory <gregory.riviere@laposte.net>
2024-02-15 19:52:16 +0100

    feat: AllerPlusLoinAvecOSAndroid - utilisation SwipeRefreshLayout - utilisation des extensions kotlin

--- AllerplusloinavecKotlin/app/src/main/java/mrs/riverjach/allerplusloinaveckotlin/activities/SwipeRefreshActivity.kt
+++ AllerplusloinavecKotlin/app/src/main/java/mrs/riverjach/allerplusloinaveckotlin/activities/SwipeRefreshActivity.kt
@@ -2,21 +2,13 @@ package mrs.riverjach.allerplusloinaveckotlin.activities
 
 import android.os.Bundle
 import android.os.Handler
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import kotlinx.android.synthetic.main.swipe_refresh_activity.swipeRefreshButton
+import kotlinx.android.synthetic.main.swipe_refresh_activity.swipeRefreshLayout
+import kotlinx.android.synthetic.main.swipe_refresh_activity.textViewSwipe
 import mrs.riverjach.allerplusloinaveckotlin.R
 
 class SwipeRefreshActivity : AppCompatActivity() {
-    val swipeRefreshLayout: SwipeRefreshLayout by lazy {
-        findViewById(R.id.swipeRefreshLayout)
-    }
-
-    val textViewSwipe: TextView by lazy {
-        findViewById(R.id.textViewSwipe)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.swipe_refresh_activity)
@@ -27,8 +19,7 @@ class SwipeRefreshActivity : AppCompatActivity() {
             stopRefresh()
         }
 
-        val button: Button = findViewById(R.id.swipeRefreshButton)
-        button.setOnClickListener {
+        swipeRefreshButton.setOnClickListener {
             swipeRefreshLayout.isRefreshing = true
             textViewSwipe.text = "Kotlin"
             stopRefresh()
