7b36e7c3b7c4feffcdf90da6988322cbecb0ac7f
RiviereGregory <gregory.riviere@laposte.net>
2024-03-29 18:26:26 +0100

    feat: affichage des données meteo

--- Meteo/app/src/main/java/mrs/riverjach/meteo/meteo/MeteoFragment.kt
+++ Meteo/app/src/main/java/mrs/riverjach/meteo/meteo/MeteoFragment.kt
@@ -1,5 +1,6 @@
 package mrs.riverjach.meteo.meteo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,6 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_meteo.city
+import kotlinx.android.synthetic.main.fragment_meteo.description
+import kotlinx.android.synthetic.main.fragment_meteo.humidity
+import kotlinx.android.synthetic.main.fragment_meteo.icon_image
+import kotlinx.android.synthetic.main.fragment_meteo.pressure
+import kotlinx.android.synthetic.main.fragment_meteo.sunrise
+import kotlinx.android.synthetic.main.fragment_meteo.sunset
+import kotlinx.android.synthetic.main.fragment_meteo.temperature
 import mrs.riverjach.meteo.App
 import mrs.riverjach.meteo.R
 import mrs.riverjach.meteo.openweather.WeatherWrapper
@@ -14,6 +24,8 @@ import mrs.riverjach.meteo.openweather.mapOpenWeatherDataToWeather
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.TimeZone
 
 class MeteoFragment : Fragment() {
     private lateinit var cityName: String
@@ -53,6 +65,7 @@ class MeteoFragment : Fragment() {
                 Log.e("Meteo Fragment", getString(R.string.city_not_load, cityName))
             }
 
+            @SuppressLint("StringFormatMatches")
             override fun onResponse(
                 call: Call<WeatherWrapper>,
                 response: Response<WeatherWrapper>
@@ -60,8 +73,26 @@ class MeteoFragment : Fragment() {
                 response.body()?.let {
                     val meteo = mapOpenWeatherDataToWeather(it)
                     Log.i("Meteo Fragment", getString(R.string.city_load, meteo))
+                    city.text = cityName
+                    Picasso.get().load(meteo.iconUrl).into(icon_image)
+                    description.text = meteo.description
+                    temperature.text = getString(R.string.temperature_city, meteo.temperature)
+                    pressure.text = getString(R.string.pressure_city, meteo.pressure)
+                    humidity.text = getString(R.string.humidity_city, "${meteo.humidity}")
+                    sunrise.text = utcToDate(meteo.sunrise)
+                    sunset.text = utcToDate(meteo.sunset)
+
                 }
             }
         })
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun utcToDate(utc: Long): String {
+        val time: String
+        val dfm = SimpleDateFormat("HH:mm:ss")
+        dfm.timeZone = TimeZone.getTimeZone("Europe/Paris")
+        time = dfm.format(java.util.Date(utc * 1000))
+        return time
     }
 }
